6fc833c8235a36d1fea2746884c9d0fc48c15302
Francisco Meglia <megliafrancisco@gmail.com>
2023-03-02 20:11:20 -0300

    Prueba de commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Probando ando");
+        System.out.println("Probando andoo");
 
         int num1 = 2 ;
         int num2 = 5;
