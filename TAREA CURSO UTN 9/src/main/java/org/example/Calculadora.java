a49f7710b1c7372b690cb78d7de0df2e6af5ccb3
Francisco Meglia <118686714+FranciscoMeglia@users.noreply.github.com>
2023-03-24 20:31:26 -0300

    Add files via upload

---
+++ TAREA CURSO UTN 9/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class Calculadora {
+
+
+    public Double sumar (double num1 , double num2) {
+        return num1 + num2;
+    }
+
+
+    public Double restar (double num1 , double num2) {
+        return num1 - num2;
+    }
+
+
+    public Double multiplicar (double num1 , double num2) {
+        return num1 * num2;
+    }
+
+
+    public Double dividir (double num1 , double num2) {
+        return num1 / num2;
+    }
+}
